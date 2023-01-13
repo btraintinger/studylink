b581c20caff4bea597226c83085e060f7c81e434
Leander Schatzmann <leanderschatzmann@gmail.com>
2023-01-13 09:23:11 +0100

    fix user icon

--- src/pages/admin/students/index.tsx
+++ src/pages/admin/students/index.tsx
@@ -1,10 +1,35 @@
-import { Typography } from '@mui/material';
-import Layout from '../../../components/page/layout';
+import { ListItem } from '@mui/material';
+import { useRouter } from 'next/router';
+import { OFFERS_STUDENT, REQUESTS_STUDENT } from '../../constants/menu-items';
 
-export default function Students() {
+interface ItemRequestOfferProps {
+  itemType: 'REQUEST' | 'OFFER';
+  id: number;
+  schoolSubject: string;
+  teacher: string;
+  grade: number;
+}
+
+export default function Students({
+  itemType,
+  id,
+  schoolSubject,
+  teacher,
+  grade,
+}: ItemRequestOfferProps) {
+  const router = useRouter();
+
+  let destinationRoute: string;
+  if ((itemType = 'OFFER')) destinationRoute = OFFERS_STUDENT;
+  else destinationRoute = REQUESTS_STUDENT;
+  destinationRoute += `/${id}`;
+
   return (
-    <Layout role="ADMIN">
-      <Typography>Students</Typography>
-    </Layout>
+    <ListItem
+      sx={{ margin: 8, padding: 8 }}
+      onClick={() => router.push(destinationRoute)}
+    >
+      Fach: {schoolSubject} Lehrer: {teacher} Jahr: {grade}
+    </ListItem>
   );
 }
