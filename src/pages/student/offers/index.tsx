09663068ceddb0111fed0ebef7ad31f5d9a042b7
Leander Schatzmann <leanderschatzmann@gmail.com>
2023-01-14 17:06:11 +0100

    add dummy files for project presentation

--- src/pages/student/offers/index.tsx
+++ src/pages/student/offers/index.tsx
@@ -1,65 +1,64 @@
-import { gql, useQuery } from '@apollo/client';
-import { Typography, ListItem } from '@mui/material';
-import Layout from '../../../components/page/layout';
-import LoadingPage from '../../../components/utils/loadingPage';
-import { useRouter } from 'next/router';
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import { useEffect, useState } from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { Box, Typography } from '@mui/material';
+import Layout from '../../../components/page/layout';
 
-const OFFERS_QUERY = gql`
-  query GetStudentOfCurrentUser {
-    getStudentOfCurrentUser {
-      tutorOfferings {
-        description
-        grade
-        id
-        teacher
-        schoolSubject {
-          extendedName
-          name
-          id
-        }
-      }
-    }
-  }
-`;
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: 'Greiml', firstName: 'Thomas', fach: 'Mathematik' },
+  { id: 2, lastName: 'Schatzmann', firstName: 'Max', fach: 'Mathematik' },
+  { id: 3, lastName: 'Weilch', firstName: 'Thomas', fach: 'Mathematik' },
+  { id: 4, lastName: 'Öttl', firstName: 'Sophie', fach: 'Mathematik' },
+  { id: 5, lastName: 'Sas', firstName: 'Tom', fach: 'Mathematik' },
+  { id: 6, lastName: 'Themen', firstName: 'Axel', fach: 'Mathematik' },
+  { id: 7, lastName: 'Scheisner', firstName: 'Lola', fach: 'Mathematik' },
+  {
+    id: 8,
+    lastName: 'Rasslkopf',
+    firstName: 'Raskolnikov',
+    fach: 'Mathematik',
+  },
+  { id: 9, lastName: 'Templer', firstName: 'Teter', fach: 'Mathematik' },
+  { id: 10, lastName: 'Tacker', firstName: 'Sara', fach: 'Mathematik' },
+  { id: 11, lastName: 'Senshuber', firstName: 'Lazarus', fach: 'Mathematik' },
+];
 
 export default function Offers() {
-  const { data, loading, error } = useQuery(OFFERS_QUERY);
-  const [array, setArray] = useState([]);
-
-  useEffect(() => {
-    if (data) {
-      setArray(data.getStudentOfCurrentUser.tutorOfferings.teacher);
-    }
-  }, [data]);
-
-  if (loading)
-    return (
-      <Layout role="STUDENT">
-        <LoadingPage></LoadingPage>
-      </Layout>
-    );
-
   return (
     <Layout role="STUDENT">
-      <Typography>Offers</Typography>
-      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        <nav aria-label="main mailbox folders">
-          <List>
-            {array.map(function (array) {
-              return <ListItem> {array} </ListItem>;
-            })}
-          </List>
-        </nav>
+      <Typography variant="h5" mb={2}>
+        Zur Vefügung stehende Nachhilfelehrer
+      </Typography>
+      <Box sx={{ width: '100%', maxWidth: 8000, bgcolor: 'background.paper' }}>
+        <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+          />
+        </div>
       </Box>
     </Layout>
   );
--- src/pages/student/requests/index.tsx
+++ src/pages/student/requests/index.tsx
@@ -1,39 +1,65 @@
-import { gql, useQuery } from '@apollo/client';
-import { Typography } from '@mui/material';
+import * as React from 'react';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { Box, Typography } from '@mui/material';
 import Layout from '../../../components/page/layout';
-import LoadingPage from '../../../components/utils/loadingPage';
 
-const OFFERS_QUERY = gql`
-  query GetSchoolClassesOfSchool {
-    getStudentOfCurrentUser {
-      tutorOfferings {
-        description
-        grade
-        id
-        teacher
-        schoolSubject {
-          extendedName
-          name
-          id
-        }
-      }
-    }
-  }
-`;
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: GridValueGetterParams) =>
+      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+  },
+];
 
-export default function Requests() {
-  const { data, loading, error } = useQuery(OFFERS_QUERY);
+const rows = [
+  { id: 1, lastName: 'Greiml', firstName: 'Thomas', fach: 'Mathematik' },
+  { id: 2, lastName: 'Schatzmann', firstName: 'Max', fach: 'Mathematik' },
+  { id: 3, lastName: 'Weilch', firstName: 'Thomas', fach: 'Mathematik' },
+  { id: 4, lastName: 'Öttl', firstName: 'Sophie', fach: 'Mathematik' },
+  { id: 5, lastName: 'Sas', firstName: 'Tom', fach: 'Mathematik' },
+  { id: 6, lastName: 'Themen', firstName: 'Axel', fach: 'Mathematik' },
+  { id: 7, lastName: 'Scheisner', firstName: 'Lola', fach: 'Mathematik' },
+  {
+    id: 8,
+    lastName: 'Rasslkopf',
+    firstName: 'Raskolnikov',
+    fach: 'Mathematik',
+  },
+  { id: 9, lastName: 'Templer', firstName: 'Teter', fach: 'Mathematik' },
+  { id: 10, lastName: 'Tacker', firstName: 'Sara', fach: 'Mathematik' },
+  { id: 11, lastName: 'Senshuber', firstName: 'Lazarus', fach: 'Mathematik' },
+];
 
-  if (loading)
-    return (
-      <Layout role="STUDENT">
-        <LoadingPage></LoadingPage>
-      </Layout>
-    );
-
+export default function Offers() {
   return (
     <Layout role="STUDENT">
-      <Typography>Requests</Typography>
+      <Typography variant="h5" mb={2}>
+        Diese Menschen wollen Nachhilfe
+      </Typography>
+      <Box sx={{ width: '100%', maxWidth: 8000, bgcolor: 'background.paper' }}>
+        <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+          />
+        </div>
+      </Box>
     </Layout>
   );
 }
