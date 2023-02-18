06bc5158f6e65bddaa9acb56e63a83931522b31b
Leander Schatzmann <leanderschatzmann@gmail.com>
2023-02-18 12:25:37 +0100

    finish accepting request dialog

--- src/components/utils/RequestDialog.tsx
+++ src/components/utils/RequestDialog.tsx
@@ -14,28 +14,51 @@ import { blue } from '@mui/material/colors';
 import PersonIcon from '@mui/icons-material/Person';
 import AddIcon from '@mui/icons-material/Add';
 import { RequestListItem } from '../../pages/student/requests';
+import { StayPrimaryLandscape } from '@mui/icons-material';
+import { useTheme } from '@emotion/react';
 
 export interface RequestDialogProps {
   open: boolean;
   selectedRow: RequestListItem;
-  onClose: () => void;
+  onClose: (acceptedItem:RequestListItem | null) => void;
 }
 
 export function RequestDialog(props: RequestDialogProps) {
   const { onClose, selectedRow, open } = props;
+  const theme = useTheme();
 
   const handleClose = () => {
-    onClose();
+    onClose(null);
   };
   const handleListItemClick = () => {
-    onClose();
+    onClose(selectedRow);
   };
-
   if (selectedRow !== null && selectedRow !== undefined) {
     return (
       <Dialog onClose={handleClose} open={open}>
-        <DialogTitle>Nachhilfe Anfrage</DialogTitle>
-        <Box>{selectedRow.grade}</Box>
+        <DialogTitle sx={{bgcolor: '#4caf50'}}>Nachhilfe Anfrage</DialogTitle>
+        <List>
+          <ListItem>
+          <Typography>Fach: </Typography>
+            <Typography> ({selectedRow.schoolSubjectName}) {selectedRow.schoolSubjectLongName}</Typography>
+          </ListItem>
+          <ListItem>
+          <Typography>Lehrer: </Typography>
+            <Typography> ({selectedRow.teacherName}) {selectedRow.teacherLongName}</Typography>
+          </ListItem>
+          <ListItem>
+          <Typography>Klasse: </Typography>
+            <Typography> {selectedRow.grade}</Typography>
+          </ListItem>
+          <ListItem>
+          <Typography>Beschreibung: </Typography>
+            <Typography> {selectedRow.description}</Typography>
+          </ListItem>
+          <ListItemButton sx={{bgcolor:'#4caf50'}} onClick={handleListItemClick}>
+            <AddIcon sx={{color: '#ffffff'}}/>
+            <Typography sx={{color: '#ffffff'}}>Akzeptieren</Typography>
+          </ListItemButton>
+        </List>
       </Dialog>
     );
   }
